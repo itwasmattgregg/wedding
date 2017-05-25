998c3e208518e88d021c2d6e86f626aafa6bd240
Matt Gregg <mgregg@spydertrap.com>
2017-05-25 09:42:53 -0500

    Link hover color and home info

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,9 +12,20 @@
 </div>
 <section class="information">
 	<div class="container">
-		<h2 class="text-center">Information</h2>
-		<div>
+		<h2 class="text-center">
+            Matt &amp; Rachel <br>
+            <small>October 7th, 2017 ~ Minneapolis, MN</small>
+        </h2>
+		<div class="row">
+            <div class="col-sm-4 info-box">
+                <h3></h3>
+            </div>
+            <div class="col-sm-4 info-box">
+                <h3>Location</h3>
+            </div>
+            <div class="col-sm-4 info-box">
 
+            </div>
 		</div>
 	</div>
 </section>
